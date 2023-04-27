188c8dfb79f9e43ffbb2499aaea039e9143dd54d
cooper <cooper.elliott30@gmail.com>
2023-04-27 18:46:22 +1000

    update folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import './styles/App.css';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Personal from './components/Personal';
+import Input from './components/Input';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <Input />
+      </section>
+      <section>
+        <Personal />
+        <Experience />
+        <Education />
+      </section>
+    </div>
+  );
 }
 
 export default App;
